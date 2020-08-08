27dcfb4f66266452f43e572989a3beafd3bd682c
GodfreySam <godfreysam09@gmail.com>
2020-08-08 01:32:42 +0100

    Updates

---
+++ precache-manifest.73bb3fa87eb37b630ddacbfcddf89c2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f152bd2f738449db44129eab40db873",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "72f86e659614657a2c64",
+    "url": "/markdown-previewer/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "a8c8be5db1024c2d414c",
+    "url": "/markdown-previewer/static/css/main.83ff1e6f.chunk.css"
+  },
+  {
+    "revision": "72f86e659614657a2c64",
+    "url": "/markdown-previewer/static/js/2.ccc557eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.ccc557eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c8be5db1024c2d414c",
+    "url": "/markdown-previewer/static/js/main.e4046295.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
